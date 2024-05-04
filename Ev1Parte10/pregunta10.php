4fd6de76cf35eca9e1316e73c3299569fdd2a450
CarelTz <81244617+Carel22@users.noreply.github.com>
2024-05-04 18:35:38 -0400

    Add files via upload
    Versiones: 
    Visual Studio Code --> Version: 1.82.2 (user setup)
    Xampp v3.3.0
    Php 8.2.4
    Boostrap: 5.3.3 Online

---
+++ Ev1Parte10/pregunta10.php
@@ -0,0 +1,19 @@
+<?php
+echo"Hola Mundo";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pregunta10</title>
+    <style>
+        body {
+            background-color: yellow;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
